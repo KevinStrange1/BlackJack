68ab5d2e405a9f77439da620959904eb994a5059
pockets1000 <104583305+pockets1000@users.noreply.github.com>
2022-11-29 12:17:19 +0000

    Update Blackjack.js

--- Blackjack.js
+++ Blackjack.js
@@ -8,10 +8,13 @@ let playerResult = 0;
 let dealerAceLogic = 0;
 let playerAceLogic = 0;
 
+let hitCard = true;
+
 window.onload = function () {
     buildDeck();
     shuffle(deck);
     startGame();
+    document.getElementById("player-sum").innerText = playerResult;
 }
 
 function buildDeck() {
@@ -74,7 +77,76 @@ function startGame() {
        document.getElementById("dealer-cards").append(cardImg);
    }
    console.log(dealerResult);
+
+   for (let i = 0; i < 2; i++) {
+        let cardImg = document.createElement('Img');
+        let card = deck.pop();
+        cardImg.src = './Images/' + card + '.png';
+        playerResult += cardValues(card);
+        playerAceLogic += checkAce(card);
+        document.getElementById("player-cards").append(cardImg);
+   }
+   document.getElementById("hit").addEventListener('click', hit);
+   document.getElementById("stay").addEventListener('click', stay);
+   console.log(playerResult);
 }
+
+function hit () {
+    if (!hitCard) {
+        return;
+    }
+    let cardImg = document.createElement('Img');
+        let card = deck.pop();
+        cardImg.src = './Images/' + card + '.png';
+        playerResult += cardValues(card);
+        playerAceLogic += checkAce(card);
+        document.getElementById("player-cards").append(cardImg);
+
+    if (reduceAce(playerResult, playerAceLogic) > 21) {
+        hitCard = false;
+
+    // document.getElementById("player-sum").innerText = playerResult;
+     //console.log(playerResult);
+    }
+}
+
+function stay () {
+    dealerResult = reduceAce(dealerResult, dealerAceLogic);
+    playerResult = reduceAce(playerResult, playerAceLogic);
+
+    hitCard = false;
+    document.getElementById('hidden').src="./Images/" + hidden + ".png";
+
+    let result = "";
+    if (playerResult > 21) {
+        result = "You Lose!";
+    }
+    else if (dealerResult > 21) {
+        result = "You Win!";
+    }
+    else if (playerResult == dealerResult) {
+        result = "Draw!";
+    }
+    else if (playerResult > dealerResult) {
+        result = "You Win!";
+    }
+    else if (playerResult < dealerResult) {
+        result = "You Lose!";
+    }
+    document.getElementById("result").innerText = result;
+    document.getElementById("dealer-sum").innerText = dealerResult;
+    document.getElementById("player-sum").innerText = playerResult;
+    console.log(playerResult);
+}
+
+function reduceAce (playerResult, playerAceLogic) {
+    while (playerResult > 21 && playerAceLogic) {
+        playerAceLogic -= 1;
+    }
+    return playerResult;
+}
+
+
 
 
 
